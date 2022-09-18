d50f5d4dff5a3cb73e5713de0e2fef1bfc3753ed
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-09-18 13:58:09 +0200

    Rating component UI.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Filters = () => {
+
+    const [rate, setRate] = useState(2);
+
   return (
     <div className='filters'>
         <span className='title'>Filter Products</span>
@@ -41,10 +45,14 @@ const Filters = () => {
                 id={`inline-4`}
             />
         </span>
-        {/* <span>
+        <span>
             <label style={{ paddingRight: 10 }}>Rating: </label>
-            <Rating rating={byRating} style={{ cursor: "pointer" }} />
-        </span> */}
+            <Rating
+                rating={rate}
+                onClick={(i) => setRate(i + 1)}
+                style={{ cursor: "pointer" }}
+                />
+        </span>
         <Button variant="light">Clear Filters</Button>
     </div>
   )
