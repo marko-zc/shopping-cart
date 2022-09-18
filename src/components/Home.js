7393dfe1e0a687ba8718c45b20c085ce447e042a
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-09-18 13:02:37 +0200

    Home page UI.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { CartState } from '../context/Context';
+import SingleProduct from './SingleProduct';
+import "./styles.css";
 
 const Home = () => {
+
+  const { state: { products } } = CartState();
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      {/* <Filters/> */}
+      <div className='productContainer'>
+        {products.map((prod) => {
+            return <SingleProduct prod={prod} key={prod.id} />;
+          })}
+      </div>
+    </div>
   )
 }
 
